58a5ab88a6b080f4b67addc511b0a992d070b378
Roderick Romero <rjrr507@gmail.com>
2020-06-21 20:08:56 -0500

    proyecto 1 terminado

--- controllers/estudiantes/index.js
+++ controllers/estudiantes/index.js
@@ -12,15 +12,90 @@ module.exports = {
       });
     } else {
       res.json({
-        success: true,
+        success: false,
         message: "campos nombre, apellido, email son requerido",
         error: "Datos vacíos",
       });
     }
   },
-  borrarEstudiante: function (req, res) {},
-  actualizarEstudiante: function (req, res) {},
-  buscarEstudiante: function (req, res) {},
+  borrarEstudiante: function (req, res) {
+    const { id } = req.params;
+
+    if (id) {
+      const data = model.deleteById(id);
+      if (data) {
+        res.json({
+          success: true,
+          message: "Estudiante Eliminado con éxito",
+        });
+      } else {
+        res.json({
+          success: false,
+          message: "Id de estudiante no existe",
+          error: "no existe",
+        });
+      }
+    } else {
+      res.json({
+        success: false,
+        message: "id de estudiante requerido",
+        error: "id requerido",
+      });
+    }
+  },
+  actualizarEstudiante: function (req, res) {
+    const { nombre, apellido, email } = req.body;
+    const id = req.body.id || req.params.id;
+
+    if (id && email && nombre && apellido) {
+      const data = model.updateItem({ id, nombre, apellido, email });
+      if (data) {
+        res.json({
+          success: true,
+          message: "se actualizo con éxito",
+          data,
+        });
+      } else {
+        res.json({
+          success: true,
+          message: "No se encontró estudiante con este ID",
+          data,
+        });
+      }
+    } else {
+      res.json({
+        success: false,
+        message: "id, nombre, apellido, email de estudiante son requeridos ",
+        error: "id requerido",
+      });
+    }
+  },
+  buscarEstudiante: function (req, res) {
+    const { id } = req.params;
+
+    if (id) {
+      const data = model.findById(id);
+      if (data) {
+        res.json({
+          success: true,
+          message: " éxito",
+          data,
+        });
+      } else {
+        res.json({
+          success: true,
+          message: "No se encontraron estudiantes con este ID",
+          data,
+        });
+      }
+    } else {
+      res.json({
+        success: false,
+        message: "id de estudiante requerido",
+        error: "id requerido",
+      });
+    }
+  },
   buscarTodos: function (req, res) {
     const data = model.getData();
     res.status(200).json({
--- model/index.js
+++ model/index.js
@@ -1,15 +1,64 @@
+const uuid = require("uuid").v4;
+
 const db = {
   data: [],
+  generateId: function () {
+    return uuid();
+  },
   getData: function () {
     return this.data;
   },
-  findById: function (id) {},
-  deleteById: function (id) {},
+  findById: function (id) {
+    let indexItem = 0;
+    const item = this.data.find((item, i) => {
+      if (item.id == id) {
+        indexItem = i;
+        return item;
+      }
+    });
+
+    if (item) return item;
+    else return false;
+  },
+  deleteById: function (id) {
+    let indexItem = 0;
+    const item = this.data.find((item, i) => {
+      if (item.id == id) {
+        indexItem = i;
+        return item;
+      }
+    });
+
+    if (item) {
+      this.data.splice(indexItem, 1);
+      return this.data;
+    } else {
+      return false;
+    }
+  },
   createItem: function (data) {
-    this.data.push(data);
+    let id = this.generateId();
+    console.log("Id generado", id);
+    this.data.push({ ...data, id });
     return this.data;
   },
-  updateItem: function (id, data) {},
+  updateItem: function (data) {
+    const { id } = data;
+    let indexItem = 0;
+    const item = this.data.find((item, i) => {
+      if (item.id == id) {
+        indexItem = i;
+        return item;
+      }
+    });
+
+    if (item) {
+      this.data[indexItem] = data;
+      return this.data[indexItem];
+    } else {
+      return false;
+    }
+  },
 };
 
 module.exports = db;
